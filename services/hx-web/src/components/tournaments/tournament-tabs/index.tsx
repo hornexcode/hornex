c12600347745c5e2a6ac0950c5730375285eecbc
Pedro <pedro357bm@gmail.com>
2023-07-11 12:17:05 -0300

    chore: setup project

--- services/hx-web/src/components/tournaments/tournament-tabs/index.tsx
+++ services/hx-web/src/components/tournaments/tournament-tabs/index.tsx
@@ -2,5 +2,5 @@ export * from "./tournament-tab";
 export * from "./overview";
 export * from "./rules";
 export * from "./teams";
-export * from "./HowItWorks";
+export * from "./how-it-works";
 export * from "./prizes";
