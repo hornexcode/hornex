4e4069dc82024229065ae3024175369c0c540b1b
Herbert Araújo <herbertaraujo.contact@gmail.com>
2023-07-14 17:40:30 -0300

    feat: new game page background style

--- services/hx-web/src/app/(core)/[gameSlug]/tournaments/page.tsx
+++ services/hx-web/src/app/(core)/[gameSlug]/tournaments/page.tsx
@@ -5,6 +5,8 @@ import { TournamentListItem } from '@/components/tournaments/tournament-list-ite
 
 import TV4 from '@/assets/images/tournaments/tournament-v4.jpg';
 import { LolIcon, LolLogoIcon } from '@/components/ui/icons';
+import Button from '@/components/ui/button/button';
+import { ArrowDownTrayIcon } from '@heroicons/react/20/solid';
 
 type GamePageProps = {
   params: {
@@ -14,136 +16,90 @@ type GamePageProps = {
 
 export default function GamePage({ params }: GamePageProps) {
   return (
-    <div className="mx-auto space-y-12">
-      {/* <div className="border-b border-slate-800 pb-2">
-        <h4 className="text-left text-xl font-bold leading-4 -tracking-wider text-white lg:text-xl">
-          News
-        </h4>
-      </div> */}
-
-      {/* News */}
-      {/* <section id="news" className="pb-8">
-        <div className="grid h-[340px] grid-cols-2 gap-4 rounded-lg">
-          <Link
-            href={""}
-            className="group relative h-full w-full overflow-hidden rounded-lg "
-          >
-            <figure className="flex h-full w-full flex-col justify-end rounded-lg bg-[url('http://localhost:3000/images/tournament-news.png')] bg-cover bg-no-repeat transition-transform group-hover:scale-105"></figure>
-            <figcaption className="absolute bottom-0 flex h-full flex-col items-start justify-end  bg-gradient-to-t from-light-dark/70 from-20% to-transparent p-6">
-              <h4 className="mb-4 text-3xl font-extrabold leading-4 -tracking-wider text-white">
-                LOL: New platform for tournaments
-              </h4>
-              <span className="text-sm text-gray-400">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Repellendus, suscipit.
-              </span>
-            </figcaption>
-          </Link>
-          <div className="grid grid-cols-2 gap-4">
-            <Link
-              href={""}
-              className="group relative h-full w-full overflow-hidden rounded-lg"
-            >
-              <figure className="flex h-full w-full flex-col justify-end rounded-lg bg-[url('http://localhost:3000/images/jinks.jpg')] bg-cover bg-no-repeat transition-transform group-hover:scale-105"></figure>
-              <figcaption className="absolute bottom-0 bg-gradient-to-t from-light-dark/70 from-70% to-transparent p-6">
-                <h4 className="text-lg font-extrabold leading-4 -tracking-wider text-white">
-                  LOL: New platform for tournaments
-                </h4>
-              </figcaption>
-            </Link>
-            <Link
-              href={""}
-              className="group relative h-full w-full overflow-hidden rounded-lg"
-            >
-              <figure className="flex h-full w-full flex-col justify-end rounded-lg bg-[url('http://localhost:3000/images/sion.jpg')] bg-cover bg-no-repeat transition-transform group-hover:scale-105"></figure>
-              <figcaption className="absolute bottom-0 bg-gradient-to-t from-light-dark/70 from-70% to-transparent p-6">
-                <h4 className="text-lg font-extrabold leading-4 -tracking-wider text-white">
-                  LOL: New platform for tournaments
-                </h4>
-              </figcaption>
-            </Link>
-            <Link
-              href={""}
-              className="group relative h-full w-full overflow-hidden rounded-lg"
-            >
-              <figure className="flex h-full w-full flex-col justify-end rounded-lg bg-[url('http://localhost:3000/images/gangplank.jpg')] bg-cover bg-no-repeat transition-transform group-hover:scale-105"></figure>
-              <figcaption className="absolute bottom-0 bg-gradient-to-t from-light-dark/70 from-70% to-transparent p-6">
-                <h4 className="text-lg font-extrabold leading-4 -tracking-wider text-white">
-                  LOL: New platform for tournaments
-                </h4>
-              </figcaption>
-            </Link>
-            <Link
-              href={""}
-              className="group relative h-full w-full overflow-hidden rounded-lg"
-            >
-              <figure className="flex h-full w-full flex-col justify-end rounded-lg bg-[url('http://localhost:3000/images/jinks.jpg')] bg-cover bg-no-repeat transition-transform group-hover:scale-105"></figure>
-              <figcaption className="absolute bottom-0 bg-gradient-to-t from-light-dark/70 from-70% to-transparent p-6">
-                <h4 className="text-lg font-extrabold leading-4 -tracking-wider text-white">
-                  LOL: New platform for tournaments
-                </h4>
-              </figcaption>
-            </Link>
+    <>
+      <div className="absolute inset-0 -z-10 h-full w-full bg-[url('http://localhost:3000/images/jinks.jpg')] bg-cover bg-no-repeat">
+        <div className="h-full w-full bg-dark/90"></div>
+      </div>
+      <div className="-z-20 mx-auto space-y-8  ">
+        <section className="relative flex justify-between p-8">
+          <div className=" left-8 top-8 flex items-center gap-6">
+            <LolIcon className="h-12" />
+            {/* <LolLogoIcon className="h-16 fill-white" /> */}
+            <h1 className="leading-1 font-satoshi p4 rounded-lg text-2xl font-extrabold -tracking-wider text-white md:text-4xl">
+              League of Legends
+            </h1>
           </div>
-        </div>
-      </section> */}
-      <section className="h-[240px]">
-        <div className="group relative flex h-full items-end bg-gradient-to-br from-sky-400 to-sky-300">
-          {/* <div className="absolute top-0 h-full w-full bg-sky-400/30"></div> */}
-          <div className="absolute top-0 h-full w-full bg-light-dark/40"></div>
-          <figure className="flex h-full w-full flex-col justify-end bg-[url('http://localhost:3000/images/jinks.jpg')] bg-cover bg-no-repeat"></figure>
-          <figcaption className="absolute left-8 top-8 flex gap-6">
-            <LolIcon className="h-16" />
-            <LolLogoIcon className="h-16 fill-white" />
-          </figcaption>
-        </div>
-      </section>
-
-      <section id="tournaments" className="px-8">
-        <div className="mb-4 border-b border-slate-800 pb-2">
-          <h4 className=" text-left text-xl font-bold leading-4 -tracking-wider text-white lg:text-xl">
-            Tournaments
-          </h4>
-        </div>
-        <div className="grid grid-cols-12">
-          <div className="col-span-12">
-            <ul className="grid grid-cols-1 gap-6 lg:grid-cols-4 lg:gap-4">
-              <li className="">
-                <Link
-                  className="divide-y divide-slate-700"
-                  href={`/league-of-legends/tournaments/0d6934cc-19ca-4384-ab3b-e6c6406a10d3`}
-                >
-                  <TournamentListItem tournament={{ thumbnail: TV4 }} />
-                </Link>
-              </li>
-              <li className="">
-                <Link
-                  className="divide-y divide-slate-700"
-                  href={`/league-of-legends/tournaments/0d6934cc-19ca-4384-ab3b-e6c6406a10d3`}
-                >
-                  <TournamentListItem tournament={{ thumbnail: TV4 }} />
-                </Link>
-              </li>
-              <li className="">
-                <Link
-                  className="divide-y divide-slate-700"
-                  href={`/league-of-legends/tournaments/0d6934cc-19ca-4384-ab3b-e6c6406a10d3`}
-                >
-                  <TournamentListItem tournament={{ thumbnail: TV4 }} />
-                </Link>
-              </li>
-              <li className="">
-                <Link
-                  className="divide-y divide-slate-700"
-                  href={`/league-of-legends/tournaments/0d6934cc-19ca-4384-ab3b-e6c6406a10d3`}
-                >
-                  <TournamentListItem tournament={{ thumbnail: TV4 }} />
-                </Link>
-              </li>
-            </ul>
+          <div className="space-x-2">
+            <Button
+              className="group flex h-[44px] cursor-pointer items-center justify-center rounded-lg bg-slate-800 text-center shadow-lg transition-all hover:bg-slate-700"
+              color="primary"
+              shape="rounded"
+            >
+              <div className="flex items-center gap-3 text-base font-bold leading-3 tracking-tight text-white">
+                <ArrowDownTrayIcon className="h-4" />
+                Download Game
+              </div>
+            </Button>
+            <Button
+              className="group flex h-[44px] cursor-pointer items-center justify-center rounded-lg bg-slate-800 text-center shadow-lg transition-all hover:bg-slate-700"
+              color="primary"
+              shape="rounded"
+            >
+              <div className="flex items-center gap-3 text-base font-bold leading-3 tracking-tight text-white">
+                <LolIcon className="h-4" />
+                Connect Game ID
+              </div>
+            </Button>
           </div>
-        </div>
-      </section>
-    </div>
+        </section>
+
+        <section id="tournaments" className="px-8">
+          <div className="mb-4 border-b border-slate-800 pb-2">
+            <h4 className="text-left text-xl font-bold leading-4 -tracking-wider text-white lg:text-xl">
+              Tournaments
+            </h4>
+          </div>
+          <div className="grid grid-cols-12">
+            <div className="col-span-12">
+              {/* grid-cols-1 gap-4 */}
+              <ul className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 lg:gap-4 xl:grid-cols-4 2xl:grid-cols-5 ">
+                <li className="">
+                  <Link
+                    className="divide-y divide-slate-700"
+                    href={`/league-of-legends/tournaments/0d6934cc-19ca-4384-ab3b-e6c6406a10d3`}
+                  >
+                    <TournamentListItem tournament={{ thumbnail: TV4 }} />
+                  </Link>
+                </li>
+                <li className="">
+                  <Link
+                    className="divide-y divide-slate-700"
+                    href={`/league-of-legends/tournaments/0d6934cc-19ca-4384-ab3b-e6c6406a10d3`}
+                  >
+                    <TournamentListItem tournament={{ thumbnail: TV4 }} />
+                  </Link>
+                </li>
+                <li className="">
+                  <Link
+                    className="divide-y divide-slate-700"
+                    href={`/league-of-legends/tournaments/0d6934cc-19ca-4384-ab3b-e6c6406a10d3`}
+                  >
+                    <TournamentListItem tournament={{ thumbnail: TV4 }} />
+                  </Link>
+                </li>
+                <li className="">
+                  <Link
+                    className="divide-y divide-slate-700"
+                    href={`/league-of-legends/tournaments/0d6934cc-19ca-4384-ab3b-e6c6406a10d3`}
+                  >
+                    <TournamentListItem tournament={{ thumbnail: TV4 }} />
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </section>
+      </div>
+    </>
   );
 }
