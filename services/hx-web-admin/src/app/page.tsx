d140fc45d5d12b781875fa029cd4790871b0b424
Ana Claudia Vignola <anacvignola@MacBook-Air-de-Ana.local>
2023-08-25 22:41:43 -0300

    feat: add login page

--- services/hx-web-admin/src/app/page.tsx
+++ services/hx-web-admin/src/app/page.tsx
@@ -1,8 +1,39 @@
+import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>teste</h1>
+    <main className="flex min-h-screen min-w-full flex-col items-center justify-center">
+      <Card>
+        <CardHeader className="space-y-1">
+          <CardTitle className="text-2xl text-center">Login</CardTitle>
+          <CardDescription>
+            Enter your email below to login your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="grid gap-4">
+          <div className="grid gap-2">
+            <Label htmlFor="email">Email</Label>
+            <Input id="email" type="email" placeholder="m@example.com" />
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="password">Password</Label>
+            <Input id="password" type="password" />
+          </div>
+        </CardContent>
+        <CardFooter>
+          <Button className="w-full">Login</Button>
+        </CardFooter>
+      </Card>
     </main>
   )
 }
