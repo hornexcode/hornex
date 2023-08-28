4b9bc280676b59e069bdb655c55cf1405af7c85a
Herbert Araújo <herbertaraujo.contact@gmail.com>
2023-08-28 15:52:27 -0300

    fix: team routes slash behavior

--- services/hx-app/hxapp/urls.py
+++ services/hx-app/hxapp/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path("api/v1/token/refresh", TokenRefreshView.as_view(), name="token_refresh"),
     # api/v1
     path("api/v1/users", include("users.urls")),
-    path("api/v1/teams/", include("teams.urls")),
+    path("api/v1/teams", include("teams.urls")),
     path("api/v1/invites", include("invites.urls")),
     path("api/v1/tournaments", include("tournaments.urls")),
 ]
