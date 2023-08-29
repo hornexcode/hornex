a396e65cd91e64954778cb9b180b44931e03377b
Herbert Araújo <herbertaraujo.contact@gmail.com>
2023-08-29 14:45:06 -0300

    chore: list method can be filtered by platform and game

--- services/hx-app/teams/views.py
+++ services/hx-app/teams/views.py
@@ -1,8 +1,5 @@
 from rest_framework import viewsets
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.permissions import IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
 
 from teams.models import Team, TeamInvite
 from teams.serializers import TeamSerializer, TeamInviteSerializer
@@ -12,6 +9,9 @@ class TeamViewSet(viewsets.ModelViewSet):
     queryset = Team.objects.all()
     serializer_class = TeamSerializer
     lookup_field = "id"
+
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ["platform", "game"]
 
 
 class TeamInviteViewSet(viewsets.ModelViewSet):
