27873cc8be862dcbb2ded324323866def2fb7348
Herbert Araújo <herbertaraujo.contact@gmail.com>
2023-09-18 12:24:37 -0300

    feat: mock riot client

--- services/hx-app/lib/hornex/riot/client.py
+++ services/hx-app/lib/hornex/riot/client.py
@@ -1,6 +1,10 @@
 from typing import List, Dict, Union, Optional
 from abc import ABC, abstractmethod
+import random
 
+from enum import Enum
+import uuid
+from django.utils import timezone
 
 # Tournament regions: BR, EUNE, EUW, JP, LAN, LAS, NA, OCE, PBE, RU, TR
 
@@ -74,7 +78,7 @@ class Clientable(ABC):
         pass
 
     @abstractmethod
-    def get_lobby_events_by_tournament_code(self, tournament_code: str) -> Dict:
+    def get_lobby_events_by_tournament_code(self, tournament_code: str) -> List[Dict]:
         """
         Return lobby data by tournament code
 
@@ -82,14 +86,53 @@ class Clientable(ABC):
         """
         pass
 
+    @abstractmethod
+    def get_a_summoner_by_summoner_name(self, name: str, region: str) -> Dict:
+        """
+        Get a summoner by summoner name.
+
+        GET /lol/summoner/v4/summoners/by-name/{summonerName}
+        """
+        pass
+
+    @abstractmethod
+    def get_league_by_summoner_id(self, id: str, region: str) -> List[Dict]:
+        """
+        Get league entries in all queues for a given summoner ID.
+
+        GET /lol/league/v4/entries/by-summoner/{encryptedSummonerId}
+        """
+        pass
+
+
+class PickType(Enum):
+    BLIND_PICK = "BLIND_PICK"
+    DRAFT_MODE = "DRAFT_MODE"
+    ALL_RANDOM = "ALL_RANDOM"
+    TOURNAMENT_DRAFT = "TOURNAMENT_DRAFT"
+
+
+class MapType(Enum):
+    SUMMONERS_RIFT = "SUMMONERS_RIFT"
+    TWISTED_TREELINE = "TWISTED_TREELINE"
+    HOWLING_ABYSS = "HOWLING_ABYSS"
+
+
+class SpectatorType(Enum):
+    NONE = "NONE"
+    LOBBYONLY = "LOBBYONLY"
+    ALL = "ALL"
+
 
 # Move this to test folder
 class TestApi(Clientable):
     def register_tournament_provider(self, url: str, region: str) -> int:
-        return 0
+        provider = random.randint(1, 99999)
+        return provider
 
     def register_tournament(self, name: str, provider_id: int) -> int:
-        return 0
+        provider = random.randint(1, 99999)
+        return provider
 
     def get_tournament_codes(
         self,
@@ -98,13 +141,63 @@ class TestApi(Clientable):
         team_size: int,
         allowed_summoner_ids: List[str],
     ) -> List[str]:
-        return []
+        codes = []
+
+        for i in range(count):
+            codes.append(str(uuid.uuid4()))
+            """ {
+                "allowedSummonerIds": allowed_summoner_ids,
+                "mapType": MapType.HOWLING_ABYSS,
+                "metadata": "Dummy tournament code metadata",
+                "pickType": PickType.BLIND_PICK,
+                "spectatorType": SpectatorType.LOBBYONLY,
+                "teamSize": team_size,
+            } """
+
+        return codes
 
     def get_match_data_by_tournament_code(self, tournament_code: str) -> Dict:
         return {}
 
-    def get_lobby_events_by_tournament_code(self, tournament_code: str) -> Dict:
-        return {}
+    def get_lobby_events_by_tournament_code(self, tournament_code: str) -> List[Dict]:
+        return [
+            {
+                "timestamp": str(timezone.now()),
+                "eventType": "Dummy event type",
+                "summonerId": uuid.uuid49,
+            }
+        ]
+
+    def get_a_summoner_by_summoner_name(self, name: str, region: str) -> Dict:
+        return {
+            "id": str(uuid.uuid4()),
+            "accountId": str(uuid.uuid4()),
+            "puuid": str(uuid.uuid4()),
+            "name": summoner_name,
+            "profileIconId": 1234,
+            "revisionDate": str(timezone.now()),
+            "summonerLevel": 30,
+        }
+
+    def get_league_by_summoner_id(self, id: str, region: str) -> List[Dict]:
+        return [
+            {
+                "leagueId": str(uuid.uuid4()),
+                "summonerId": summoner_id,
+                "summonerName": "TestSummoner",
+                "queueType": "RANKED_SOLO_5x5",
+                "tier": "DIAMOND",
+                "rank": "I",
+                "leaguePoints": 100,
+                "wins": 50,
+                "losses": 30,
+                "hotStreak": False,
+                "veteran": False,
+                "freshBlood": False,
+                "inactive": False,
+                "miniSeries": {"target": 3, "wins": 1, "losses": 1, "progress": "WLN"},
+            }
+        ]
 
 
 def getApi(api_key: str) -> Clientable:
