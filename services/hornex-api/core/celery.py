d91de1202181a1dd17d3c699902c86ebcc64b5ca
Pedro <pedro357bm@gmail.com>
2023-12-09 15:20:02 -0300

    feat(polaris): Install Tilt and create Dockerfile for hornex-api (#72)

--- services/hornex-api/core/celery.py
+++ services/hornex-api/core/celery.py
@@ -4,6 +4,6 @@ import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 django.setup()
-app = Celery("store_app", broker="pyamqp://guest@localhost//")
+app = Celery("consumer_app", broker="pyamqp://guest@localhost//")
 
 app.autodiscover_tasks()
