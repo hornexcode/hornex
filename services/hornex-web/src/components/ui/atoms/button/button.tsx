5cfeb3f84dc39c22c6404bad7790a48af2ecf68b
Pedro <pedro357bm@gmail.com>
2023-12-22 09:14:09 -0300

    chore: improved layout on login page

--- services/hornex-web/src/components/ui/atoms/button/button.tsx
+++ services/hornex-web/src/components/ui/atoms/button/button.tsx
@@ -128,7 +128,7 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(
         ref={buttonRef}
         onClick={clickHandler}
         className={cn(
-          'relative inline-flex shrink-0 items-center justify-center overflow-hidden text-center text-xs font-bold tracking-wide outline-none transition-all sm:text-[15px]',
+          'shadow-card relative inline-flex shrink-0 items-center justify-center overflow-hidden text-center text-xs font-bold tracking-wide outline-none transition-all sm:text-[15px]',
           !disabled
             ? buttonColorClassNames
             : 'cursor-not-allowed bg-gray-400 text-gray-400',
