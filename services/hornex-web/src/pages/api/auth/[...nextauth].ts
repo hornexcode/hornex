ec3acc627ddf84daab2df1ab7311b43c24fcafef
Pedro Santos <pedro357bm@gmail.com>
2024-02-03 13:37:02 -0300

    chore: setup nextauth js

--- services/hornex-web/src/pages/api/auth/[...nextauth].ts
+++ services/hornex-web/src/pages/api/auth/[...nextauth].ts
@@ -1,75 +1,20 @@
-import { LoginResponse } from '@/infra/hx-core/responses/login';
-import NextAuth, { NextAuthOptions, Session, User } from 'next-auth';
-import { AdapterUser } from 'next-auth/adapters';
-import { JWT } from 'next-auth/jwt';
-import CredentialsProvider from 'next-auth/providers/credentials';
-import GoogleProvider from 'next-auth/providers/google';
+import NextAuth, { NextAuthOptions } from 'next-auth';
+import GithubProvider from 'next-auth/providers/github';
 
 // For more information on each option (and a full list of options) go to
 // https://next-auth.js.org/configuration/options
 export const authOptions: NextAuthOptions = {
   // https://next-auth.js.org/configuration/providers/oauth
+  pages: {
+    signIn: '/auth/signin',
+  },
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
-    CredentialsProvider({
-      name: 'Credentials',
-      credentials: {
-        username: { label: 'Username', type: 'text', placeholder: 'jsmith' },
-        password: { label: 'Password', type: 'password' },
-      },
-      async authorize(credentials, req) {
-        // You need to provide your own logic here that takes the credentials
-        // submitted and returns either a object representing a user or value
-        // that is false/null if the credentials are invalid.
-        // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
-        // You can also use the `req` object to obtain additional parameters
-        // (i.e., the request IP address)
-        console.log(credentials);
-        const res = await fetch('http://localhost:9234/api/v1/auth/login', {
-          credentials: 'include',
-          method: 'POST',
-          body: JSON.stringify({
-            email: credentials?.username,
-            password: credentials?.password,
-          }),
-          headers: { 'Content-Type': 'application/json' },
-        });
-
-        const data = (await res.json()) as LoginResponse;
-
-        // If no error and we have user data, return it
-        if (res.ok && data) {
-          return data.user;
-        }
-        // Return null if user data could not be retrieved
-        return null;
-      },
-    }),
-    GoogleProvider({
-      clientId: '',
-      clientSecret: '',
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
     }),
   ],
-  callbacks: {
-    async session({
-      session,
-      token,
-    }: {
-      session: Session;
-      user: User | AdapterUser;
-      token: JWT;
-    }) {
-      if (typeof token.id === 'string') {
-        session.user.id = token.id;
-      }
-      //
-      return session;
-    },
-    async jwt({ token }) {
-      // token.userRole = 'admin';
-      console.log(token);
-      return token;
-    },
-  },
 };
 
 export default NextAuth(authOptions);
