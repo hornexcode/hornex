bc1d6ec33b9def0b59cb0451f8d27e1fc6a43315
Pedro Santos <pedro357bm@gmail.com>
2024-02-07 20:14:57 -0300

    chore: renamed league of legends models

--- services/hornex-api/apps/tournaments/models.py
+++ services/hornex-api/apps/tournaments/models.py
@@ -1,4 +1,3 @@
-import os
 import uuid
 from abc import abstractmethod
 from datetime import UTC, datetime, timedelta
@@ -14,13 +13,6 @@ from apps.games.models import GameID
 from apps.teams.models import Team
 from apps.tournaments import errors
 from apps.tournaments.validators import validate_team_size
-from lib.challonge import Tournament as ChallongeTournamentResourceAPI
-from lib.riot import (
-    Provider as RiotProviderResourceAPI,
-)
-from lib.riot import (
-    Tournament as RiotTournamentResourceAPI,
-)
 
 logger = structlog.get_logger(__name__)
 
@@ -456,7 +448,7 @@ class Checkin(models.Model):
         return f"Checkin ({self.id}) | {self.tournament.name}"
 
 
-class LeagueEntry(models.Model):
+class LeagueOfLegendsEllo(models.Model):
     class TierOptions(models.TextChoices):
         IRON = "IRON"
         BRONZE = "BRONZE"
@@ -490,14 +482,14 @@ class LeagueEntry(models.Model):
         return f"{self.tier} {self.rank}"
 
 
-class Summoner(models.Model):
+class LeagueOfLegendsSummoner(models.Model):
     game_id = models.ForeignKey(GameID, on_delete=models.CASCADE)
     id = models.CharField(max_length=255, primary_key=True, editable=False)
     puuid = models.CharField(max_length=255)
     account_id = models.CharField(max_length=255)
     name = models.CharField(max_length=255)
-    league_entry = models.ForeignKey(
-        LeagueEntry, on_delete=models.CASCADE, blank=True, null=True
+    ello = models.ForeignKey(
+        LeagueOfLegendsEllo, on_delete=models.CASCADE, blank=True, null=True
     )
 
     created_at = models.DateTimeField(auto_now_add=True)
@@ -507,7 +499,7 @@ class Summoner(models.Model):
         return self.name
 
 
-class Provider(models.Model):
+class LeagueOfLegendsProvider(models.Model):
     class RegionType(models.TextChoices):
         BR = "BR"
         EUNE = "EUNE"
@@ -553,7 +545,7 @@ class LeagueOfLegendsTournament(Tournament):
         ALL = "ALL"
 
     provider = models.ForeignKey(
-        Provider,
+        LeagueOfLegendsProvider,
         on_delete=models.DO_NOTHING,
         null=True,
         blank=True,
@@ -568,7 +560,7 @@ class LeagueOfLegendsTournament(Tournament):
     spectator = models.CharField(
         max_length=50, choices=SpectatorType.choices, default=SpectatorType.LOBBYONLY
     )
-    allowed_league_entries = models.ManyToManyField(LeagueEntry)
+    allowed_ellos = models.ManyToManyField(LeagueOfLegendsEllo)
     riot_tournament_id = models.IntegerField(null=True, blank=True)
 
     class Meta:
@@ -578,177 +570,6 @@ class LeagueOfLegendsTournament(Tournament):
         return [
             f"{entry.tier} {entry.rank}" for entry in self.allowed_league_entries.all()
         ]
-
-    def checkin(self):
-        def has_enough_participants():
-            participants_total = 0
-            for team in self.teams.all():
-                for _ in team.members.all():
-                    participants_total += 1
-
-            return self.teams.count() >= MINIMUM_PARTICIPANTS
-
-        def add_participants_to_challonge_tournament(tournament: str):
-            try:
-                ChallongeTournamentResourceAPI.add_participants(
-                    tournament=tournament,
-                    participants=[
-                        {
-                            "name": "Team 1",
-                            "seed": 1,
-                        },
-                        {
-                            "name": "Team 2",
-                            "seed": 2,
-                        },
-                        {
-                            "name": "Team 3",
-                            "seed": 3,
-                        },
-                        {
-                            "name": "Team 4",
-                            "seed": 4,
-                        },
-                        {
-                            "name": "Team 5",
-                            "seed": 5,
-                        },
-                        {
-                            "name": "Team 6",
-                            "seed": 6,
-                        },
-                        {
-                            "name": "Team 7",
-                            "seed": 7,
-                        },
-                        {
-                            "name": "Team 8",
-                            "seed": 8,
-                        },
-                        {
-                            "name": "Team 9",
-                            "seed": 9,
-                        },
-                        {
-                            "name": "Team 10",
-                            "seed": 10,
-                        },
-                        {
-                            "name": "Team 11",
-                            "seed": 11,
-                        },
-                        {
-                            "name": "Team 12",
-                            "seed": 12,
-                        },
-                        {
-                            "name": "Team 13",
-                            "seed": 13,
-                        },
-                        {
-                            "name": "Team 14",
-                            "seed": 14,
-                        },
-                        {
-                            "name": "Team 15",
-                            "seed": 15,
-                        },
-                        {
-                            "name": "Team 16",
-                            "seed": 16,
-                        },
-                    ],
-                )
-            except Exception as e:
-                logger.error("ChallongeTournamentResourceAPI.add_participants", error=e)
-                raise ValidationError(
-                    {"error": "Failed to create participants in Challonge"}
-                )
-
-        def create_challonge_tournament():
-            try:
-                chllng_trnmnt: ChallongeTournamentResourceAPI = (
-                    ChallongeTournamentResourceAPI.create(
-                        name=self.name[:60],
-                        description=self.description,
-                        tournament_type="single elimination",
-                        start_at=self.registration_start_date.isoformat(),
-                        check_in_duration=15,
-                        participants_total=self.max_teams,
-                        teams=True,
-                    )
-                )
-                self.challonge_tournament_id = chllng_trnmnt.id
-                self.challonge_tournament_url = chllng_trnmnt.full_challonge_url
-                self.save()
-
-                return chllng_trnmnt
-            except Exception as e:
-                logger.error("ChallongeTournamentResourceAPI.create", error=e)
-                raise ValidationError(
-                    {"error": "Failed to create tournament in Challonge"}
-                )
-
-        def register_leagueoflegends_tournament():
-            try:
-                riot_provider_id = Provider.objects.get(
-                    region=Provider.RegionType.BR
-                ).id
-                logger.info("Provider.objects.get successfully")
-            except Provider.DoesNotExist:
-                riot_provider_id = RiotProviderResourceAPI.create(
-                    region=Provider.RegionType.BR,
-                    url=os.getenv("HORNEX_API_BASE_URL", ""),
-                )
-                logger.info(
-                    "RiotProviderResourceAPI.create",
-                    riot_provider_id=riot_provider_id,
-                )
-                Provider.objects.create(
-                    id=riot_provider_id,
-                    region=Provider.RegionType.BR,
-                )
-                logger.info("Provider.objects.create successfully")
-            except Exception as e:
-                logger.error("Provider.objects.get", error=e)
-                raise ValidationError(
-                    {"error": "Failed to retrieve and create provider"}
-                )
-
-            try:
-                # 4. create tournament in riot
-                riot_tournament_id = RiotTournamentResourceAPI.create(
-                    name=self.name[:60],
-                    provider_id=riot_provider_id,
-                )
-
-            except Exception as e:
-                logger.error("RiotTournamentResourceAPI.create", error=e)
-                raise ValidationError({"error": "Failed to create tournament in Riot"})
-
-            self.riot_tournament_id = riot_tournament_id
-            self.save()
-
-        # -
-        # TODO: turn this into a celery task
-        if not has_enough_participants():
-            raise ValidationError(
-                {"error": "Minimum number of participants not reached"}
-            )
-
-        chllng_trnmnt = create_challonge_tournament()
-        add_participants_to_challonge_tournament(chllng_trnmnt.id)
-        register_leagueoflegends_tournament()
-
-    def member_checkin(self, user, team) -> Checkin:
-        try:
-            checkin = Checkin.objects.get(user=user, team=team, tournament=self)
-            print("Already checked in")
-            if checkin:
-                raise ValidationError("Already checked in")
-
-        except Checkin.DoesNotExist:
-            return Checkin.objects.create(user=user, team=team, tournament=self)
 
     def __str__(self) -> str:
         return f"{self.name} ({self.id})"
