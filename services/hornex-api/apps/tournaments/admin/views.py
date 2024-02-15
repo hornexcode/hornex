a7dde22e8bdab79c33ffeeaabf9d9f5d96e8503a
Pedro Santos <pedro357bm@gmail.com>
2024-02-15 20:37:36 -0300

    chore: added route to create a tournament on admin namspace

---
+++ services/hornex-api/apps/tournaments/admin/views.py
@@ -0,0 +1,14 @@
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from apps.tournaments.usecases.create_tournament import (
+    CreateTournamentUseCase,
+    CreateTournamentUseCaseParams,
+)
+
+
+@api_view(["POST"])
+def tournaments_controller(request):
+    tournament = CreateTournamentUseCase().execute(CreateTournamentUseCaseParams(**request.data))
+    return Response({"id": tournament.id}, status=status.HTTP_201_CREATED)
