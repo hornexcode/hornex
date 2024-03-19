2e11c9edfcea4e9d9ced1e589271d93d61299738
Pedro <pedro357bm@gmail.com>
2024-03-19 11:05:22 -0300

    fix: Organizers showing all tournaments (#177)

--- services/hornex-api/core/experiments.py
+++ services/hornex-api/core/experiments.py
@@ -3,10 +3,8 @@ from django.conf import settings
 
 def experimental(func):
     def wrapper(*args, **kwargs):
-        print("Experimental feature is enabled")
         if settings.TESTING:
             return
         func(*args, **kwargs)
-        print("Code run successfully!")
 
     return wrapper
