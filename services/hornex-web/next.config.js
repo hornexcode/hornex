804b10dd58558ddc4fe3ad867d2ec7714e3ff07b
Pedro <pedro357bm@gmail.com>
2024-03-23 15:16:30 -0300

    hotfix: removed i18n config from web service (#184)

--- services/hornex-web/next.config.js
+++ services/hornex-web/next.config.js
@@ -2,10 +2,6 @@ const nextConfig = {
   images: {
     domains: ['localhost:3000'],
   },
-  i18n: {
-    locales: ['en-US', 'fr', 'pt-BR'],
-    defaultLocale: 'en-US',
-  },
 };
 
 module.exports = nextConfig;
--- services/hornex-web/ni18n.config.ts
+++ services/hornex-web/ni18n.config.ts
@@ -2,6 +2,6 @@
 import type { Ni18nOptions } from 'ni18n';
 
 export const ni18nConfig: Ni18nOptions = {
-  supportedLngs: ['en', 'pt', 'fr'],
+  supportedLngs: ['en'],
   ns: ['common'],
 };
