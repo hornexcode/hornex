5029ac2553b20c516f1dc9f8b4758d5f8c6f0366
Pedro Santos <pedro357bm@gmail.com>
2024-04-09 21:02:27 -0300

    chore: give permissions to aws pem file

--- services/hornex-api/scripts/deploy.sh
+++ services/hornex-api/scripts/deploy.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-
-
 REPO_ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")" && git rev-parse --show-toplevel)
 source ${REPO_ROOT}/devops/scripts/helpers
 
 info "Deploying api..."
 
 echo "${AWS_PEM_FILE}" > "${REPO_ROOT}/hornex-api.pem"
+chmod 400 "${REPO_ROOT}/hornex-api.pem"
 
 # Connect to the ec2 instance
 ssh -i "${REPO_ROOT}/hornex-api.pem" $AWS_EC2_HOSTNAME << EOF
@@ -32,3 +31,6 @@ ssh -i "${REPO_ROOT}/hornex-api.pem" $AWS_EC2_HOSTNAME << EOF
       exit 1
     fi
     echo "Successfully deployed the api"
+EOF
+
+info "Successfully deployed the api"
