fb99fc8d9bc028644022e14b829992d1da7f074c
Pedro Santos <pedro357bm@gmail.com>
2024-04-09 21:06:47 -0300

    fix: github do not support terminal commands run

--- services/hornex-api/scripts/deploy.sh
+++ services/hornex-api/scripts/deploy.sh
@@ -10,27 +10,30 @@ echo "${AWS_PEM_FILE}" > "${REPO_ROOT}/hornex-api.pem"
 chmod 400 "${REPO_ROOT}/hornex-api.pem"
 
 # Connect to the ec2 instance
-ssh -i "${REPO_ROOT}/hornex-api.pem" $AWS_EC2_HOSTNAME << EOF
-    cd hornex-api
-    git pull
-    if [ $? -ne 0 ]; then
-      error "Failed to pull the latest changes from the repository"
-      exit 1
-    fi
+ssh -i "${REPO_ROOT}/hornex-api.pem" $AWS_EC2_HOSTNAME
+if [ $? -ne 0 ]; then
+  error "Failed to connect to the ec2 instance"
+  exit 1
+fi
 
-    cd services/hornex-api
-    ./.venv/bin/python3 manage.py migrate
-    if [ $? -ne 0 ]; then
-      error "Failed to apply the migrations"
-      exit 1
-    fi
+cd hornex-api
+git pull
+if [ $? -ne 0 ]; then
+  error "Failed to pull the latest changes from the repository"
+  exit 1
+fi
 
-    sudo systemctl restart gunicorn
-    if [ $? -ne 0 ]; then
-      error "Failed to restart the gunicorn service"
-      exit 1
-    fi
-    echo "Successfully deployed the api"
-EOF
+cd services/hornex-api
+./.venv/bin/python3 manage.py migrate
+if [ $? -ne 0 ]; then
+  error "Failed to apply the migrations"
+  exit 1
+fi
+
+sudo systemctl restart gunicorn
+if [ $? -ne 0 ]; then
+  error "Failed to restart the gunicorn service"
+  exit 1
+fi
 
 info "Successfully deployed the api"
